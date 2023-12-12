7b9d0376566a84c4b194179df5822a9adb1ffda8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-12 19:51:31 +0500

    Depen

---
+++ JWT-Permition/src/JWT.Application/DependecyInjection.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace JWT.Application
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
