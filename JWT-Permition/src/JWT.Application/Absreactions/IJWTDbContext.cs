c93beebec4f23f7027480999051463f3d77ba703
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-12 19:51:24 +0500

    JWT

---
+++ JWT-Permition/src/JWT.Application/Absreactions/IJWTDbContext.cs
@@ -0,0 +1,14 @@
+﻿using JWT.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWT.Application.Absreactions
+{
+    public interface IJWTDbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
