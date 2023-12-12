fe41d06f07f31d35611a09aacbb0095f1d47ef9a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-12 19:53:20 +0500

    RolePermission

---
+++ JWT-Permition/src/JWT.Domain/Entities/RolePermission.cs
@@ -0,0 +1,10 @@
+﻿namespace JWT.Domain.Entities
+{
+    public class RolePermission
+    {
+        public int RolePermissionId { get; set; }
+        public int RoleId { get; set; }
+        public int PermissionId { get; set; }
+
+    }
+}
