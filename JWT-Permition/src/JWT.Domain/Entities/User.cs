48eaf02f424442c4bac99dff98ea17445cd9de5c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-12 19:53:25 +0500

    User

---
+++ JWT-Permition/src/JWT.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace JWT.Domain.Entities;
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string Salt { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpireDate { get; set; }
+
+    public List<Role> Roles { get; set; }
+}
