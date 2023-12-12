945c22baad754415e0e00407f81fbf1dc1d4a89d
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-12 19:54:09 +0500

    djkua dshjavfi ai

---
+++ JWT-Permition/src/JWT.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,18 @@
+﻿using JWT.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JWT.Infrastructure;
+public static class DependecyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<JWTDbContext, JWTDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("Defoult"));
+        });
+
+        return services;
+    }
+}
