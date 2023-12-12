f9411aad7fe5d094d0251185378a94187672ed6f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-12 19:52:52 +0500

    Role

---
+++ JWT-Permition/src/JWT.Domain/Entities/Role.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JWT.Domain.Entities;
+public class Role
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    [JsonIgnore]
+    public List<User> Users { get; set; }
+    public List<Permission> Permissions { get; set; }
+
+}
