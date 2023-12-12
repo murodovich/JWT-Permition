c4f0baba08acace2551f41b1fca74a0f8c5e659b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-12 19:51:15 +0500

    program

---
+++ JWT-Permition/src/JWT.Api/Program.cs
@@ -0,0 +1,28 @@
+using JWT.Application;
+using JWT.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
