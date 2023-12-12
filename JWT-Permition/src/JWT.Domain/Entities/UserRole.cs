ce3e43382efac7dec493839b04f8f84b086170d0
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-12 19:53:31 +0500

    UserRole

---
+++ JWT-Permition/src/JWT.Domain/Entities/UserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace JWT.Domain.Entities
+{
+    public class UserRole
+    {
+        public int UserRoleId { get; set; }
+        public int RoleId { get; set; }
+        public int UserId { get; set; }
+    }
+}
